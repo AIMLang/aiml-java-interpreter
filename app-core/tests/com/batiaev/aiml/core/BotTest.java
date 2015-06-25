79439276d030a0f02005c5ffb1a3d6b17cfed438
Anton Batiaev <a.batyaev@gmail.com>
2015-06-25 21:56:47 +0400

    #AJI-3 support srai

--- app-core/tests/com/batiaev/aiml/core/BotTest.java
+++ app-core/tests/com/batiaev/aiml/core/BotTest.java
@@ -23,10 +23,25 @@ public class BotTest extends Assert {
     }
 
     @Test
-    public void testMultisentenceRespond2() throws Exception {
+    public void testMultisentenceRespondWithRandom() throws Exception {
         AIMLConst.setRootPath(".");
         Bot bot = new Bot();
         String request = "Привет";
+        String respond = bot.multisentenceRespond(request, new ChatState("Human")).trim();
+        String[] answers = "Здравствуй;Здравствуйте;Мое почтение!;Здарова;Приветствую;Привет;Доброго времени суток".split(";");
+        boolean result = false;
+        for (String answer : answers) {
+            if (respond.equals(answer))
+                result = true;
+        }
+        assertTrue("Request = " + request + ", Respond = " + respond, result);
+    }
+
+    @Test
+    public void testMultisentenceRespondWithSrai() throws Exception {
+        AIMLConst.setRootPath(".");
+        Bot bot = new Bot();
+        String request = "Здравствуй";
         String respond = bot.multisentenceRespond(request, new ChatState("Human")).trim();
         String[] answers = "Здравствуй;Здравствуйте;Мое почтение!;Здарова;Приветствую;Привет;Доброго времени суток".split(";");
         boolean result = false;
